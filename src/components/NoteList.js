4ced0addc060bdd5c54d3b1da45f0da8916e5993
EbizimohAbodei <vincentkendy@gmail.com>
2022-05-02 19:16:32 +0300

    Fixed table errors

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -5,24 +5,28 @@ const NodeList = (props) => {
     <div className="noteList">
       <p className="title">Data from local server</p>
       <table>
-        <tr class="tableHead">
-          <th>ID</th>
-          <th>Firstname</th>
-          <th>Lastname</th>
-          <th>Phone</th>
-          <th>Role</th>
-          <th>Message</th>
-        </tr>
-        {props.data.map((listItem) => (
-          <tr key={listItem.id}>
-            <td>{listItem.id}</td>
-            <td>{listItem.firstname}</td>
-            <td>{listItem.lastname}</td>
-            <td>{listItem.phone}</td>
-            <td>{listItem.role}</td>
-            <td>{listItem.message}</td>
+        <thead>
+          <tr className="tableHead">
+            <th>ID</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Phone</th>
+            <th>Role</th>
+            <th>Message</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {props.data.map((listItem) => (
+            <tr key={listItem.id}>
+              <td>{listItem.id}</td>
+              <td>{listItem.firstname}</td>
+              <td>{listItem.lastname}</td>
+              <td>{listItem.phone}</td>
+              <td>{listItem.role}</td>
+              <td>{listItem.message}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
